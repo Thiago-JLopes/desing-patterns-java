48f2ffdab16a4b75a663dda2ef69da6e38afd1b4
Thiago Lopes <tiagojosecnn@outlook.com>
2024-06-23 00:05:12 -0300

    refatora.

--- src/main/java/com/dio/projeto/design_patterns/controller/UserController.java
+++ src/main/java/com/dio/projeto/design_patterns/controller/UserController.java
@@ -7,34 +7,36 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("us")
+@RequestMapping("/user")
 public class UserController {
+
     @Autowired
     private UserService userService;
 
     @GetMapping
-    public ResponseEntity<Iterable<Usuario>> buscarTodos(){
+    public ResponseEntity<Iterable<Usuario>> buscarTodos() {
         return ResponseEntity.ok(userService.buscarTodos());
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Usuario> buscarPorId(Long id){
+    public ResponseEntity<Usuario> buscarPorId(@PathVariable Long id) {
         return ResponseEntity.ok(userService.buscarPorId(id));
     }
 
     @PostMapping
-    public ResponseEntity<Usuario> inserir(Usuario usuario){
+    public ResponseEntity<Usuario> inserir(@RequestBody Usuario usuario) {
         userService.inserir(usuario);
         return ResponseEntity.ok(usuario);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Usuario> atualizar(Long id, Usuario usuario){
+    public ResponseEntity<Usuario> atualizar(@PathVariable Long id, @RequestBody Usuario usuario) {
         userService.atualizar(id, usuario);
         return ResponseEntity.ok(usuario);
     }
+
     @DeleteMapping("/{id}")
-    public ResponseEntity<Usuario> deletar(Long id){
+    public ResponseEntity<Void> deletar(@PathVariable Long id) {
         userService.deletar(id);
         return ResponseEntity.ok().build();
     }
